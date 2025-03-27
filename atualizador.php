69dbccc18a79297808145ee4e0a59856dd7fd34c
Matheus Assunção <matheus.tba@hotmail.com>
2025-03-27 13:53:39 +0000

    test git webhook

--- atualizador.php
+++ atualizador.php
@@ -1,3 +1,3 @@
 <?php
 
-file_put_contents(__DIR__ . '/log.txt', date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
+file_put_contents(__DIR__ . '/log.txt', '[' . date('Y-m-d H:i:s') . ']  Executado' . PHP_EOL, FILE_APPEND);
