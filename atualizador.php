43c4a5cc67bc16d5146927e614561697ae0d0fd2
Matheus Assunção <matheus.tba@hotmail.com>
2025-03-27 14:00:01 +0000

    test git webhook

--- atualizador.php
+++ atualizador.php
@@ -1,3 +1,7 @@
 <?php
 
 file_put_contents(__DIR__ . '/log.txt', '[' . date('Y-m-d H:i:s') . ']  Executado' . PHP_EOL, FILE_APPEND);
+
+define('USER_AUTH', 'admin');
+define('PASS_AUTH', '123456');
+define('API_URL', 'https://api.com.br');
